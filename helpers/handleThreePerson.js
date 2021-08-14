849707878944c20a1fde1a3c067e919cdb0de717
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-08-14 12:52:05 -0700

    Added three person conde outline

---
+++ helpers/handleThreePerson.js
@@ -0,0 +1,171 @@
+const inquirer = require("inquirer");
+
+threeOne = () => {
+    console.log("Question 3.01");
+    inquirer.prompt([
+        {
+            name: 3.01,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeTwo = () => {
+    console.log("Question 3.02");
+    inquirer.prompt([
+        {
+            name: 3.02,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeThree = () => {
+    console.log("Question 3.03");
+    inquirer.prompt([
+        {
+            name: 3.03,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeFour = () => {
+    console.log("Question 3.04");
+    inquirer.prompt([
+        {
+            name: 3.04,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeFive = () => {
+    console.log("Question 3.05");
+    inquirer.prompt([
+        {
+            name: 3.05,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeSix = () => {
+    console.log("Question 3.06");
+    inquirer.prompt([
+        {
+            name: 3.06,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeSeven = () => {
+    console.log("Question 3.07");
+    inquirer.prompt([
+        {
+            name: 3.07,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeEight = () => {
+    console.log("Question 3.08");
+    inquirer.prompt([
+        {
+            name: 3.08,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeNine = () => {
+    console.log("Question 3.09");
+    inquirer.prompt([
+        {
+            name: 3.09,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeTen = () => {
+    console.log("Question 3.10");
+    inquirer.prompt([
+        {
+            name: 3.10,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeEleven = () => {
+    console.log("Question 3.11");
+    inquirer.prompt([
+        {
+            name: 3.11,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeTwelve = () => {
+    console.log("Question 3.12");
+    inquirer.prompt([
+        {
+            name: 3.12,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeThirteen = () => {
+    console.log("Question 3.13");
+    inquirer.prompt([
+        {
+            name: 3.13,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+threeFourteen = () => {
+    console.log("Question 3.14");
+    inquirer.prompt([
+        {
+            name: 3.14,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+module.exports = {threeOne, threeTwo, threeThree, threeFour, threeFive, threeSix, threeSeven, threeEight, threeNine, threeTen, threeEleven, threeTwelve, threeThirteen, threeFourteen};
