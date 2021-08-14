a06880328f2d6a11cab93e492ac4598469a00b69
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-08-14 13:02:17 -0700

    added one person outline

---
+++ helpers/handleOnePerson.js
@@ -0,0 +1,75 @@
+const inquirer = require("inquirer");
+
+oneOne = () => {
+    console.log("Question 1.01");
+    inquirer.prompt([
+        {
+            name: 1.01,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+oneTwo = () => {
+    console.log("Question 1.02");
+    inquirer.prompt([
+        {
+            name: 1.02,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+oneThree = () => {
+    console.log("Question 1.03");
+    inquirer.prompt([
+        {
+            name: 1.03,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+oneFour = () => {
+    console.log("Question 1.04");
+    inquirer.prompt([
+        {
+            name: 1.04,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+oneFive = () => {
+    console.log("Question 1.05");
+    inquirer.prompt([
+        {
+            name: 1.05,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+oneSix = () => {
+    console.log("Question 1.06");
+    inquirer.prompt([
+        {
+            name: 1.06,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+module.exports = {oneOne, oneTwo, oneThree, oneFour, oneFive, oneSix};
