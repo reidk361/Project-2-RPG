07ac35977435e442940204990be3c19e9b85b6e3
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-08-14 12:57:31 -0700

    added two person outline

---
+++ helpers/handleTwoPerson.js
@@ -0,0 +1,124 @@
+const inquirer = require("inquirer");
+
+twoOne = () => {
+    console.log("Question 2.01");
+    inquirer.prompt([
+        {
+            name: 2.01,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoTwo = () => {
+    console.log("Question 2.02");
+    inquirer.prompt([
+        {
+            name: 2.02,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoThree = () => {
+    console.log("Question 2.03");
+    inquirer.prompt([
+        {
+            name: 2.03,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoFour = () => {
+    console.log("Question 2.04");
+    inquirer.prompt([
+        {
+            name: 2.04,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoFive = () => {
+    console.log("Question 2.05");
+    inquirer.prompt([
+        {
+            name: 2.05,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoSix = () => {
+    console.log("Question 2.06");
+    inquirer.prompt([
+        {
+            name: 2.06,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoSeven = () => {
+    console.log("Question 2.07");
+    inquirer.prompt([
+        {
+            name: 2.07,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoEight = () => {
+    console.log("Question 2.08");
+    inquirer.prompt([
+        {
+            name: 2.08,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoNine = () => {
+    console.log("Question 2.09");
+    inquirer.prompt([
+        {
+            name: 2.09,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+twoTen = () => {
+    console.log("Question 2.10");
+    inquirer.prompt([
+        {
+            name: 2.10,
+            type: "list",
+            message: "",
+            choices: ['', '', '',]
+        },
+    ])
+}
+
+
+module.exports = {twoOne, twoTwo, twoThree, twoFour, twoFive, twoSix, twoSeven, twoEight, twoNine, twoTen};
